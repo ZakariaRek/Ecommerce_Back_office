8720a36b5bf7fad6fd4bdc81455e9099c10840ae
ZakariaRekhla2004 <zakariaest49@gmail.com>
2025-07-04 17:46:02 +0100

    update steps 2 in product

--- src/pages/Product/Product/ProductList.tsx
+++ src/pages/Product/Product/ProductList.tsx
@@ -715,7 +715,7 @@ export default function ProductList() {
 
   if (loading) {
     return (
-      <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-purple-50 dark:from-gray-900 dark:via-blue-900 dark:to-purple-900 p-6">
+      <div className="min-h-screen bg-gradient-to-br  from-slate-50 via-blue-50 to-purple-50 dark:from-gray-900 dark:via-blue-900 dark:to-purple-900 p-6">
         <div className="max-w-12xl mx-auto">
           <div className="floating-card rounded-3xl overflow-hidden backdrop-blur-xl">
             <div className="product-gradient text-white p-8 relative overflow-hidden">
